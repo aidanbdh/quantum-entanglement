46065c142685cf798f6c683f5437f557741f1e1d
aidanbdh <aidanbdh1998@gmail.com>
2017-02-08 16:09:01 -0800

    Created socket.io conenction that sends and recieves events across the connection #2

--- spec/quantum-entanglement-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import QuantumEntanglementView from '../lib/quantum-entanglement-view';
-
-describe('QuantumEntanglementView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
