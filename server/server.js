8393fcde23edf681da3d9a094485067a42e31d6d
aidanbdh <aidanbdh1998@gmail.com>
2017-02-15 13:35:02 -0800

    Added other user's cursors with colors #9

--- server/server.js
+++ server/server.js
@@ -13,9 +13,11 @@ io.on('connection', socket => {
     socket.join(name)
     if(rooms.indexOf(name) === -1) {
       rooms.push(name)
-      rooms.push({ host: socket.id })
+      rooms.push({ host: socket.id, colors: ['white', 'black', 'maroon', 'red', 'pink', 'brown', 'orange', 'coral', 'olive', 'yellow', 'beige', 'lime', 'green', 'mint', 'teal', 'cyan', 'navy', 'blue', 'purple', 'lavender', 'magenta', 'grey'] })
       console.log(`A user created ${name}`)
-      socket.emit('joined', name)
+      const num = Math.floor(Math.random() * rooms[rooms.indexOf(name) + 1].colors.length)
+      const color = rooms[rooms.indexOf(name) + 1].colors.slice(num, num + 1)
+      socket.emit('joined', { name, color})
     } else {
       socket.emit('file conflict')
     }
@@ -24,11 +26,16 @@ io.on('connection', socket => {
     })
     socket.on('send file', ({ id, file, cursors }) => {
       console.log(`A user joined ${name}`)
-      socket.to(id).emit('replace', { file, cursors })
-      socket.to(id).emit('joined', name)
+      const num = Math.floor(Math.random() * rooms[rooms.indexOf(name) + 1].colors.length)
+      const color = rooms[rooms.indexOf(name) + 1].colors.slice(num, num + 1)
+      socket.to(id).emit('replace', { file, newCursors: cursors, color })
+      socket.to(id).emit('joined', { name, color})
     })
-    socket.on('cursor moved', event => {
-      socket.broadcast.to(name).emit('update cursor', event)
+    socket.on('new socket', (cursor) => {
+      socket.broadcast.to(name).emit('new socket', cursor)
+    })
+    socket.on('cursor moved', info => {
+      socket.broadcast.to(name).emit('update cursor', info)
     })
     socket.on('leave', function() { socket.leave(name) })
   })
